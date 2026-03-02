bdb37c698fe1c6691c605da2823fbbdbae8b8a59
SaoMadao <zhassulan.estai@narxoz.kz>
2026-03-02 19:07:43 +0500

    create BattleEngine singleton skeleton

--- src/com/narxoz/rpg/battle/BattleEngine.java
+++ src/com/narxoz/rpg/battle/BattleEngine.java
@@ -1,11 +1,8 @@
 package com.narxoz.rpg.battle;
 
-import java.util.List;
-import java.util.Random;
-
 public final class BattleEngine {
+
     private static BattleEngine instance;
-    private Random random = new Random(1L);
 
     private BattleEngine() {
     }
@@ -15,24 +12,5 @@ public final class BattleEngine {
             instance = new BattleEngine();
         }
         return instance;
-    }
-
-    public BattleEngine setRandomSeed(long seed) {
-        this.random = new Random(seed);
-        return this;
-    }
-
-    public void reset() {
-        // TODO: reset any battle state if you add it
-    }
-
-    public EncounterResult runEncounter(List<Combatant> teamA, List<Combatant> teamB) {
-        // TODO: validate inputs and run round-based battle
-        // TODO: use random if you add critical hits or target selection
-        EncounterResult result = new EncounterResult();
-        result.setWinner("TBD");
-        result.setRounds(0);
-        result.addLog("TODO: implement battle simulation");
-        return result;
     }
 }
