6b0278df2403d265d25a719588b0a99163145528
SaoMadao <zhassulan.estai@narxoz.kz>
2026-03-02 19:18:59 +0500

    Добавил генератор случайных чисел и начальное значение по умолчанию

--- src/com/narxoz/rpg/battle/BattleEngine.java
+++ src/com/narxoz/rpg/battle/BattleEngine.java
@@ -1,12 +1,15 @@
 package com.narxoz.rpg.battle;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Random;
+
 public final class BattleEngine {
-
     private static BattleEngine instance;
-
-    private BattleEngine() {
-    }
-
+    private BattleEngine() {}
+    private static final long DEFAULT_SEED = 1L;
+    private Random random = new Random(DEFAULT_SEED);
     public static BattleEngine getInstance() {
         if (instance == null) {
             instance = new BattleEngine();
